7684a58429228fd3a27fe4b95eaa18d4c9276c37
asa55 <asa55@pitt.edu>
2024-03-11 01:10:12 +0000

    remove comment

--- src/GreetingState.test.js
+++ src/GreetingState.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-//import userEvent from '@testing-library/user-event'
 import Greeting from './Greeting';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 test('renders the correct greeting text after 1 click', () => {
   render(<Greeting name={""} />);
