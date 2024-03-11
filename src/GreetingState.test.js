96b5a12582d5709bebd7b2848bbd9f0706737117
asa55 <asa55@pitt.edu>
2024-03-11 04:09:54 +0000

    get element by text with initial state

--- src/GreetingState.test.js
+++ src/GreetingState.test.js
@@ -4,7 +4,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 
 test('renders the correct greeting text after 1 click', () => {
   render(<Greeting name={""} />);
-  const greetingElement = screen.getByText(/click/i);
+  const greetingElement = screen.getByText(/0/i);
   fireEvent.click(greetingElement)
   expect(greetingElement).toHaveTextContent("1")
 });
